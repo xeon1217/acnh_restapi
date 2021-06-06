4de9628ed89513d1b5f659a16dcc5742ad4ccf54
xeon1 <xeon1217@gmail.com>
2021-06-07 03:03:03 +0900

    Initial commit

---
+++ src/main/kotlin/com/jungyoon/acnh_restapi/AcnhRestapiApplication.kt
@@ -0,0 +1,11 @@
+package com.jungyoon.acnh_restapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AcnhRestapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AcnhRestapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jungyoon/acnh_restapi/AcnhRestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jungyoon.acnh_restapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AcnhRestapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
